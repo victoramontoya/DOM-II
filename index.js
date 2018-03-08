335fb38d06b8d8926f8888d49f4a6df7a1ac0524
Andrew Jarrett <ahrjarrett@gmail.com>
2018-03-07 21:59:45 -0700

    got it to work kinda

--- index.js
+++ index.js
@@ -5,18 +5,45 @@ const pinkBlock = document.querySelector('.block--pink');
 const grayBlock = document.querySelector('.block--gray');
 const parent = document.querySelector('.blocks');
 
-var counter = -1;
+var counter = 0;
 
-const rockets = (e) => {
+const rockets = e => {
     counter--;
     e.target.style.order = counter;
-    
 }
 
-//parent.addEventListener('click', rockets);
+const travelers = e => {
+  console.log('yo')
+  //e.stopPropagation()
+  e.target.style.left = '100%';
+  e.target.style.transition = 'left 0.5s';
+  e.target.style.position = 'relative';
+}
 
-redBlock.addEventListener('click', rockets);
-blueBlock.addEventListener('click', rockets);
-greenBlock.addEventListener('click', rockets);
-pinkBlock.addEventListener('click', rockets);
-grayBlock.addEventListener('click', rockets);
+const travelersUp = e => {
+  console.log('mouse up')
+  e.target.style.transition = 'left 0.5s';
+  e.target.style.left = '0%';
+}
+
+//parent.addEventListener('mouseup', travelersUp);
+
+// Commit 1:
+//redBlock.addEventListener('click', rockets);
+//blueBlock.addEventListener('click', rockets);
+//greenBlock.addEventListener('click', rockets);
+//pinkBlock.addEventListener('click', rockets);
+//grayBlock.addEventListener('click', rockets);
+
+redBlock.addEventListener('mousedown', travelers);
+blueBlock.addEventListener('mousedown', travelers);
+greenBlock.addEventListener('mousedown', travelers);
+pinkBlock.addEventListener('mousedown', travelers);
+grayBlock.addEventListener('mousedown', travelers);
+
+redBlock.addEventListener('mouseup', travelersUp);
+blueBlock.addEventListener('mouseup', travelersUp);
+greenBlock.addEventListener('mouseup', travelersUp);
+pinkBlock.addEventListener('mouseup', travelersUp);
+grayBlock.addEventListener('mouseup', travelersUp);
+
